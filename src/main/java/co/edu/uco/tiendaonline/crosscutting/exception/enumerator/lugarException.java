6ca12cc089b5f68c8986afb66c04d082223dba63
julianzuluagaaristi <140759923+julianzuluagaaristi@users.noreply.github.com>
2023-10-06 11:46:49 -0500

    Delete src directory

--- src/main/java/co/edu/uco/tiendaonline/crosscutting/exception/enumerator/lugarException.java
+++
@@ -1,6 +0,0 @@
-package co.edu.uco.tiendaonline.crosscutting.exception.enumerator;
-
-public enum lugarException {
-	DATA, SERVICE, CONTROLLER, GENERAL, CROSSCUTTING
-
-}
--- src/main/java/co/edu/uco/tiendaonline/crosscutting/messages/enumerator/TipoMensaje.java
+++
@@ -1,6 +0,0 @@
-package co.edu.uco.tiendaonline.crosscutting.messages.enumerator;
-
-public enum TipoMensaje {
-	TECNICO, USUARIO,
-
-}
